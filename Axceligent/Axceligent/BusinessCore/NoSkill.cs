023920f1618d0507d8025b56a6bf8ee0fd3063aa
soonlim77 <soonlim@gmail.com>
2018-11-10 23:24:22 +0800

    Revert "Revert "Axceligent""
    This reverts commit 802e205d3f3ed48da3656713edfb28d4a7c9b7a6.

---
+++ Axceligent/Axceligent/BusinessCore/NoSkill.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Axceligent.Interface;
+
+namespace Axceligent.BusinessCore
+{
+    public class NoSkill : ISkill
+    {
+        public NoSkill()
+        {
+
+        }
+        public string PresentSkill()
+        {
+            return "No SKill Defined";
+        }
+    }
+}
---
+++ Axceligent/Axceligent/Core/Alexa.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Axceligent.Core
+{
+    public class Alexa
+    {
+         
+        public  string ownername { get; set; }
+        public  string greetingmessage { get; set; }
+
+        public Alexa()
+        {
+            if (ownername == null)
+            {
+                ownername = "Alexa";
+                greetingmessage = "hello";
+            }
+
+
+        }
+
+        // action takes input and return void
+        public void Configure(Action<Alexa> act)
+        {
+            act(this);
+        }
+     
+        public string Talk()
+        {
+          
+            return greetingmessage + "," + ownername;
+            
+        }
+       
+
+    }
+}
